8eab4e7f0626e1376fe2afb9a496c5d964561580
mvalchanov <m.valchanov@abv.bg>
2018-03-25 22:40:01 +0300

    fixed for issue Problem 1

---
+++ intro-and-basic-syntax/exercise/problem 1/DebitCardNumber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+//CONFIRMED from mbrz
+namespace CSharpIntroAndBasicSyntax
+{
+    class DebitCardNumber
+    {
+        static void Main()
+        {
+            int buffer = 1;
+            string output = String.Empty;
+            while (buffer <= 4)
+            {
+                string strInteger = Console.ReadLine();
+                if (strInteger.Length < 4)
+                {
+                    strInteger = FormatIntegers(strInteger);
+                }
+
+                output += strInteger + " ";
+                buffer++;
+            }
+            
+            Console.WriteLine(output.Trim());
+        }
+
+        static string FormatIntegers(string integers)
+        {
+            return integers.PadLeft(4, '0');
+        }
+    }
+}
